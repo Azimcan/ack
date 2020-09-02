69e0d1d65b321852072a1a90a3116bbd4c162674
Azim Can KURUCA <azimcankuruca@gmail.com>
2020-09-02 12:24:00 +0300

    yayinlama guncellendi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
-	def index
-	end
+  def index
+    puts Rails.root.join("db/ack-data.json")
+    file = File.read(Rails.root.join("db/ack-data.json"))
+    data = JSON.parse(file)
+
+    puts data
+    @social = data["social"]
+    puts @social
+    @kit = data["kit"]
+  end
 end
