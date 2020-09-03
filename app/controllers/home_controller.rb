137f04444a2f3ee7b0bb8612bb15cd9572795cea
Azim Can KURUCA <azimcankuruca@gmail.com>
2020-09-03 13:00:37 +0300

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     puts Rails.root.join("db/ack-data.json")
-    file = File.read(Rails.root.join("db/ack-data.json"))
+    file = File.read(Rails.root.join("db/data.json"))
     data = JSON.parse(file)
 
     @info = data["info"]
